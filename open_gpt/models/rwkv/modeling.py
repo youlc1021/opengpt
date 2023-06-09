636f50963fbb2ba888f7c64fe54aa2e82df3c36b
Lechun <youlc1021@163.com>
2023-06-09 12:51:12 +0800

    encode multiple prompts

--- open_gpt/models/rwkv/modeling.py
+++ open_gpt/models/rwkv/modeling.py
@@ -1,3 +1,4 @@
+from typing import List
 
 from open_gpt.models.modeling import BaseModel
 class RWKVModel(BaseModel):
@@ -13,30 +14,18 @@ class RWKVModel(BaseModel):
                  do_sample:bool,
                  num_return_sequences:int,
                  **kwargs):
-        if self._model_name_or_path.startswith('sgugger'):
-            prompt = f"\n{prompt}"
-            inputs = self.tokenizer(prompt, return_tensors="pt")
-            output = self.model.generate(inputs["input_ids"], max_new_tokens=max_length, top_p=top_p, do_sample=do_sample)
-            output = self.tokenizer.decode(output[0].tolist())
-        else:
-            prompt = f"### Instruction: {prompt}\n### Response:"
-            inputs = self.tokenizer(prompt, return_tensors="pt")
-            output = self.model.generate(inputs["input_ids"], max_new_tokens=max_length)
-            output = self.tokenizer.decode(output[0].tolist(), skip_special_tokens=True)
+        inputs = self.tokenizer(prompt, return_tensors="pt")
+        output = self.model.generate(inputs["input_ids"], max_new_tokens=max_length)
+        output = self.tokenizer.decode(output[0].tolist(), skip_special_tokens=True)
         return output
 
-    def embedding(self, prompt: str, **kwargs):
-        if self._model_name_or_path.startswith('sgugger/rwkv'):
-            prompt = f"\n{prompt}"
-        elif self._model_name_or_path.startswith('ybelkada/rwkv'):
-            prompt = f"### Instruction: {prompt}\n### Response:"
-
+    def encode(self, prompts: List[str], **kwargs):
         embedding = []
         def layer_hook(module, inp, out):
             embedding.append(out)
 
-        inputs = self.tokenizer(prompt, return_tensors="pt")
+        inputs = self.tokenizer(prompts, return_tensors="pt",padding=True)
         hook = self.model.rwkv.embeddings.register_forward_hook(layer_hook)
-        output = self.model.forward(inputs["input_ids"])
+        outputs = self.model.forward(inputs["input_ids"])
         hook.remove()
         return embedding
